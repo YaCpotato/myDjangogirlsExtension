0aeec404342cbc9164184104338b1abe93600698
YaCpotato <yasshisshy@gmail.com>
2019-09-09 02:17:56 +0900

    タスク登録機能が完成

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('accounts/login/', v.LoginView.as_view(), name='login'),
     path('accounts/logout/', v.LogoutView.as_view(next_page='/'), name='logout'),
     path('todo/', views.task_list, name='task_list'),
+    path('todo/new/', views.task_new, name='task_new'),
+    path('todo/<int:pk>/edit/', views.task_edit, name='task_edit'),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -9,3 +9,32 @@ from django.shortcuts import render, get_object_or_404,redirect
 def task_list(request):
     tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def task_new(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.created_date = timezone.now()
+            task.save()
+            return render(request, 'todo/task_list.html', {'tasks': tasks})
+    else:
+        form = TaskForm()
+    return render(request, 'todo/task_edit.html', {'form': form})
+
+
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task_list', {'tasks': tasks})
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todo/task_edit.html', {'form': form})
